2c3a7a691913183b8c31316c5e04bd7a760c35bc
Andres Hortua <fandresh7@gmail.com>
2024-11-14 23:41:19 -0500

    add logo and backgrounds to check layout behavior

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,11 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core'
+import { SidebarService } from '../../services/sidebar.service'
+import { NgClass } from '@angular/common'
 
 @Component({
   selector: 'main-header',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  sidebarService = inject(SidebarService)
+
+  sidebar = this.sidebarService.sidebar
+}
