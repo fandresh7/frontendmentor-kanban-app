4f7a41a2366b865f38cfcb694d1db5cb69f03984
Andres Hortua <fandresh7@gmail.com>
2024-11-15 23:28:31 -0500

    finish header, fix error with toggle sidebar in header

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core'
 import { SidebarService } from '../../services/sidebar.service'
 import { NgClass } from '@angular/common'
-import { ArrowDownIconComponent, LogoIconComponent } from '../../../shared/components/icons.component'
+import { ArrowDownIconComponent, LogoIconComponent, plusIconComponent, EllipsisIconComponent } from '../../../shared/components/icons.component'
 
 @Component({
   selector: 'main-header',
   standalone: true,
-  imports: [NgClass, ArrowDownIconComponent, LogoIconComponent],
+  imports: [NgClass, ArrowDownIconComponent, LogoIconComponent, plusIconComponent, EllipsisIconComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
--- src/app/shared/components/icons.component.ts
+++ src/app/shared/components/icons.component.ts
@@ -96,6 +96,33 @@ export class SunIconComponent {}
 export class MoonIconComponent {}
 
 @Component({
+  selector: 'ellipsis-icon',
+  standalone: true,
+  template: `<svg
+    width="5"
+    height="20"
+    xmlns="http://www.w3.org/2000/svg">
+    <g
+      fill="currentColor"
+      fill-rule="evenodd">
+      <circle
+        cx="2.308"
+        cy="2.308"
+        r="2.308" />
+      <circle
+        cx="2.308"
+        cy="10"
+        r="2.308" />
+      <circle
+        cx="2.308"
+        cy="17.692"
+        r="2.308" />
+    </g>
+  </svg>`
+})
+export class EllipsisIconComponent {}
+
+@Component({
   selector: 'hide-icon',
   standalone: true,
   template: `<svg
